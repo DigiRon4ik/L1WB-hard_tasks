ab5fdc064554e99ca86fbaa90fa8a729565d85c2
DigiRon4ik <dr.digiron@gmail.com>
2025-01-12 22:28:13 +0300

    "8 «Or channel»" is done

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -5,8 +5,8 @@ package main
 
 Реализовать функцию, которая будет объединять один или более done каналов в single канал если один из его составляющих каналов закроется.
 Одним из вариантов было бы очевидно написать выражение при помощи select, которое бы реализовывало эту связь,
-однако иногда неизестно общее число done каналов, с которыми вы работаете в рантайме.
-В этом случае удобнее использовать вызов единственной функции, которая, приняв на вход один или более or каналов, реализовывала весь функционал.
+однако иногда неизвестно общее число done каналов, с которыми вы работаете в рантайме.
+В этом случае удобнее использовать вызов единственной функции, которая, приняв на вход один или более or-каналов, реализовывала весь функционал.
 
 Определение функции:
 var or func(channels ...<- chan interface{}) <- chan interface{}
@@ -30,9 +30,75 @@ start := time.Now()
 	sig(1*time.Minute),
 )
 
-fmt.Printf(“fone after %v”, time.Since(start))
+fmt.Printf(“fine after %v”, time.Since(start))
 */
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+// or combines several done channels into one.
+// If at least one of the channels is closed, then the resulting channel is also closed.
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	// Step 1: Process the base cases.
+	switch len(channels) {
+	case 0:
+		return nil // If there are no channels, return nil.
+	case 1:
+		return channels[0] // If there is only one channel, we simply return it.
+	}
+
+	// Step 2: Create the resulting channel.
+	orDone := make(chan interface{})
+
+	go func() { // Launch the goroutine.
+		defer close(orDone) // The goroutine will close the orDone channel upon completion.
+
+		// Step 3: Divide the channels into groups and listen to them.
+		switch len(channels) {
+		case 2: // If there are 2 channels, listen to them using select.
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default: // If there are more channels, we divide them in half.
+			m := len(channels) / 2
+			select {
+			case <-or(channels[:m]...): // Call it recursively for the first half.
+			case <-or(channels[m:]...): // Call it recursively for the second half.
+			}
+		}
+	}()
+
+	return orDone // Return the resulting channel.
 }
+
+func main() {
+	// An example of the sig helper function.
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{}) // Create a channel.
+		go func() {
+			defer close(c)    // Close the channel when we finish work.
+			time.Sleep(after) // “Fall asleep” for a specified time.
+		}()
+		return c
+	}
+
+	// Testing the or function.
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("Done after %v\n", time.Since(start))
+}
+
+/*
+ - Output: -
+Done after 1.0004678s
+*/
--- develop/dev07/task_test.go
+++ develop/dev07/task_test.go
@@ -1,1 +1,94 @@
 package main
+
+import (
+	"testing"
+	"time"
+)
+
+// An auxiliary function for creating a closing channel after a specified time.
+func sig(after time.Duration) <-chan interface{} {
+	c := make(chan interface{})
+	go func() {
+		defer close(c)
+		time.Sleep(after)
+	}()
+	return c
+}
+
+// Test: If one channel is transmitted, the result must be the same channel.
+func TestOr_SingleChannel(t *testing.T) {
+	done := make(chan interface{})
+	close(done)
+
+	result := or(done)
+
+	select {
+	case <-result:
+	case <-time.After(1 * time.Second):
+		t.Fatalf("or did not close when single channel closed")
+	}
+}
+
+// Test: If two channels are transmitted, or should close when either one is closed.
+func TestOr_TwoChannels(t *testing.T) {
+	ch1 := make(chan interface{})
+	ch2 := make(chan interface{})
+
+	go func() {
+		time.Sleep(100 * time.Millisecond)
+		close(ch1)
+	}()
+
+	result := or(ch1, ch2)
+
+	select {
+	case <-result:
+	case <-time.After(1 * time.Second):
+		t.Fatalf("or did not close when one of two channels closed")
+	}
+}
+
+// Test: If multiple channels are transmitted, or should close when the first one closes.
+func TestOr_MultipleChannels(t *testing.T) {
+	channels := []<-chan interface{}{
+		sig(500 * time.Millisecond),
+		sig(100 * time.Millisecond),
+		sig(1 * time.Second),
+	}
+
+	start := time.Now()
+	result := or(channels...)
+
+	<-result
+	duration := time.Since(start)
+
+	if duration < 100*time.Millisecond || duration > 200*time.Millisecond {
+		t.Fatalf("or closed too late or too early: %v", duration)
+	}
+}
+
+// Test: or should work with an empty channel list (result nil).
+func TestOr_EmptyChannels(t *testing.T) {
+	result := or()
+	if result != nil {
+		t.Fatalf("or did not return nil for empty input")
+	}
+}
+
+// Test: Testing performance with a large number of channels.
+func TestOr_ManyChannels(t *testing.T) {
+	var channels []<-chan interface{}
+	for i := 0; i < 1000; i++ {
+		channels = append(channels, sig(10*time.Millisecond))
+	}
+
+	start := time.Now()
+	result := or(channels...)
+
+	<-result
+	duration := time.Since(start)
+
+	if duration > 20*time.Millisecond {
+		t.Fatalf("or took too long for many channels: %v", duration)
+	}
+}
